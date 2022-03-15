a4d9f12aa326396eea47a6c394af29ed4e6adb3d
BritishPandaChick <sbartley1080@gmail.com>
2022-03-15 16:32:13 -0400

    Fix README. Add comments

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ var red = [0, 100, 63];
 var orange = [0, 100, 63];
 var green = [75, 100, 40];
 var blue = [196, 77, 55];
-var purple = [280, 50, 60];
+var purple = [280, 50, 60];
+
 var myName = "Name";
 
-var letterColors = [red, orange, green, blue, purple];if (10 < 3) {
-  bubbleShape = "square";
-} else {  bubbleShape = "circle";}
+var letterColors = [red, orange, green, blue, purple];
 
-drawName(myName, letterColors);bounceBubbles();
+if (10 < 3) {
+  bubbleShape = "square";
+} else {
+  bubbleShape = "circle";
+}
+
+drawName(myName, letterColors);
+bounceBubbles();
